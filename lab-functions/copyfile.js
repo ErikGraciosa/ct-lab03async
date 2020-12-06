e60b69f69c2a625e1196f0a0677ff6e335c4fa23
ErikGraciosa <erikgraciosa@gmail.com>
2020-12-06 10:44:30 -0800

    copy function passing test

--- lab-functions/copyfile.js
+++ lab-functions/copyfile.js
@@ -1,17 +1,13 @@
 const fsPromises = require('fs').promises;
 
-
-
 const copy = async (source, destination) => {
     try {
         await fsPromises.readFile(source, 'utf-8')
-            .then(data => fsPromises.writeFile(destination, data));
-            console.log('copy is done');
+            .then(data => fsPromises.writeFile(destination, data))
+            .then(console.log('copy is done'));
     } catch(err) {
         console.log(err);
     }
 }
-
-copy('README.md', 'CopyOfREADME.md' );
 
 module.exports = { copy }
