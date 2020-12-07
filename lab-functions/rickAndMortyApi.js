c70631a5166d6b0caef32d33449d0d2e54de2217
ErikGraciosa <erikgraciosa@gmail.com>
2020-12-06 16:36:31 -0800

    getCharacter test failing

--- lab-functions/rickAndMortyApi.js
+++ lab-functions/rickAndMortyApi.js
@@ -1,5 +1,11 @@
-const getCharacter = (id) => {
-    return 'not content';
+const fetch = require('node-fetch');
+
+const getCharacter = async (id) => {
+    const data = await fetch(`https://rickandmortyapi.com/api/character/${id}`)
+        .then(res => res.json());
+    console.log(data);
 }
+
+getCharacter(5);
 
 module.exports = { getCharacter };
--- lab-functions/rickAndMortyApi.test.js
+++ lab-functions/rickAndMortyApi.test.js
@@ -1,15 +1,14 @@
 const { getCharacter } = require('./rickAndMortyApi');
 
-
-
 const id = 5;
 
 describe('Testing verifies getCharacter function', () => {
-
-    
-  
     it('test will call getCharacter with parameter id', async () => {
       const character = await getCharacter(id);
-      expect(character).toEqual('content');
+      expect(character).toEqual({ 
+          "name": "Jerry Smith",
+          "status": "Alive",
+          "species": "Human"
+      });
     });  
   });
